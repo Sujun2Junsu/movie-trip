2033b891f5a9ea1d8bb1daeb9dcdf966ac549466
tjatn304905 <tjatn304905@gmail.com>
2021-05-23 17:05:33 +0900

    tmdb api 얻어오는 함수 및 json파일 수정

---
+++ server/movies/tmdb.py
@@ -0,0 +1,197 @@
+import requests, json
+from .models import Movie
+from django.http import HttpResponse
+
+def create_movies(request):
+
+    results = []
+   
+    ### popular 40 ###
+    # data 받아오기
+    my_url = 'https://api.themoviedb.org/3/movie/popular?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR'
+    response = requests.get(my_url)
+    movie_dict = response.json()
+
+    # 필요한 정보 골라내기
+    movie_list = movie_dict.get('results')
+    for movie in movie_list:
+        title = movie.get('original_title')
+        overview = movie.get('overview')
+        release_date = movie.get('release_date')
+        poster_path = movie.get('poster_path')
+        popularity = movie.get('popularity')
+
+        # DB에 저장하기, 중복제거
+        (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
+
+        result = {
+            'model': "movies.movie",
+            # 'pk': len(results) + 1,
+            "fields": {
+            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
+            'title': title,
+            'overview': overview,
+            'release_date': release_date,
+            'popularity': popularity
+        }}
+        if result not in results:
+            results.append(result)
+
+    # data 받아오기
+    my_url = 'https://api.themoviedb.org/3/movie/popular?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR&page=2'
+    response = requests.get(my_url)
+    movie_dict = response.json()
+
+    # 필요한 정보 골라내기
+    movie_list = movie_dict.get('results')
+    for movie in movie_list:
+        title = movie.get('original_title')
+        overview = movie.get('overview')
+        release_date = movie.get('release_date')
+        poster_path = movie.get('poster_path')
+        popularity = movie.get('popularity')
+
+        # DB에 저장하기, 중복제거
+        (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
+
+        result = {
+            'model': "movies.movie",
+            # 'pk': len(results) + 1,
+            "fields": {
+            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
+            'title': title,
+            'overview': overview,
+            'release_date': release_date,
+            'popularity': popularity
+        }}
+        if result not in results:
+            results.append(result)
+
+    ### now_playing 40 ###
+    # data 받아오기
+    my_url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR'
+    response = requests.get(my_url)
+    movie_dict = response.json()
+
+    # 필요한 정보 골라내기
+    movie_list = movie_dict.get('results')
+    for movie in movie_list:
+        title = movie.get('original_title')
+        overview = movie.get('overview')
+        release_date = movie.get('release_date')
+        poster_path = movie.get('poster_path')
+        popularity = movie.get('popularity')
+
+        # DB에 저장하기, 중복제거
+        (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
+
+        result = {
+            'model': "movies.movie",
+            # 'pk': len(results) + 1,
+            "fields": {
+            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
+            'title': title,
+            'overview': overview,
+            'release_date': release_date,
+            'popularity': popularity
+        }}
+        if result not in results:
+            results.append(result)
+    
+    # data 받아오기
+    my_url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR&page=2'
+    response = requests.get(my_url)
+    movie_dict = response.json()
+
+    # 필요한 정보 골라내기
+    movie_list = movie_dict.get('results')
+    for movie in movie_list:
+        title = movie.get('original_title')
+        overview = movie.get('overview')
+        release_date = movie.get('release_date')
+        poster_path = movie.get('poster_path')
+        popularity = movie.get('popularity')
+
+        # DB에 저장하기, 중복제거
+        (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
+
+        result = {
+            'model': "movies.movie",
+            # 'pk': len(results) + 1,
+            "fields": {
+            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
+            'title': title,
+            'overview': overview,
+            'release_date': release_date,
+            'popularity': popularity
+        }}
+        if result not in results:
+            results.append(result)
+
+    ### top_rated 40 ###
+    # data 받아오기
+    my_url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR'
+    response = requests.get(my_url)
+    movie_dict = response.json()
+
+    # 필요한 정보 골라내기
+    movie_list = movie_dict.get('results')
+    for movie in movie_list:
+        title = movie.get('original_title')
+        overview = movie.get('overview')
+        release_date = movie.get('release_date')
+        poster_path = movie.get('poster_path')
+        popularity = movie.get('popularity')
+
+        # DB에 저장하기, 중복제거
+        (movie, created) = Movie.objects.update_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
+        
+        result = {
+            'model': "movies.movie",
+            # 'pk': len(results) + 1,
+            "fields": {
+            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
+            'title': title,
+            'overview': overview,
+            'release_date': release_date,
+            'popularity': popularity
+        }}
+        if result not in results:
+            results.append(result)
+
+    # data 받아오기
+    my_url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR&page=2'
+    response = requests.get(my_url)
+    movie_dict = response.json()
+
+    # 필요한 정보 골라내기
+    movie_list = movie_dict.get('results')
+    for movie in movie_list:
+        title = movie.get('original_title')
+        overview = movie.get('overview')
+        release_date = movie.get('release_date')
+        poster_path = movie.get('poster_path')
+        popularity = movie.get('popularity')
+
+        # DB에 저장하기, 중복제거
+        (movie, created) = Movie.objects.update_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
+        
+        result = {
+            'model': "movies.movie",
+            # 'pk': len(results) + 1,
+            "fields": {
+            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
+            'title': title,
+            'overview': overview,
+            'release_date': release_date,
+            'popularity': popularity
+        }}
+        if result not in results:
+            results.append(result)
+    
+    print(len(results))
+
+    with open('movies/fixtures/movies/movies.json', "w", encoding='utf-8') as f: # movies.json은 저장될 json 파일 명입니다., json_data은 저장할 데이터입니다.
+        json.dump(results, f, ensure_ascii=False)
+
+    return HttpResponse(status=200)
--- server/movies/views.py
+++ server/movies/views.py
@@ -3,107 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.response import Response
 
-import requests, pprint, json
+import requests
 from django.shortcuts import render, redirect, get_list_or_404
 from .serializers import MovieListSerializer, MovieSerializer
 from .models import Movie
-
-def create_movies(request):
-
-    results = []
-   
-    # data 받아오기
-    # https://api.themoviedb.org/3/movie/now_playing?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR
-    # https://api.themoviedb.org/3/movie/top_rated?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR
-    # https://api.themoviedb.org/3/movie/popular?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR
-    my_url = 'https://api.themoviedb.org/3/movie/popular?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR'
-    response = requests.get(my_url)
-    movie_dict = response.json()
-    # return Response(movie_dict)
-
-    # 필요한 정보 골라내기
-    movie_list = movie_dict.get('results')
-    for movie in movie_list:
-        title = movie.get('original_title')
-        overview = movie.get('overview')
-        release_date = movie.get('release_date')
-        poster_path = movie.get('poster_path')
-        popularity = movie.get('popularity')
-
-        # # DB에 저장하기, 중복제거
-        # (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
-
-        results.append({
-            "model": "movies.movie",
-            "pk": len(results) + 1,
-            "fields": {
-            "poster_path": "https://image.tmdb.org/t/p/w500" +  poster_path,
-            "title": title,
-            "overview": overview,
-            "release_date": release_date,
-            "popularity": popularity
-        }})
-
-    my_url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR'
-    response = requests.get(my_url)
-    movie_dict = response.json()
-    # return Response(movie_dict)
-
-    # 필요한 정보 골라내기
-    movie_list = movie_dict.get('results')
-    for movie in movie_list:
-        title = movie.get('original_title')
-        overview = movie.get('overview')
-        release_date = movie.get('release_date')
-        poster_path = movie.get('poster_path')
-        popularity = movie.get('popularity')
-
-        # # DB에 저장하기, 중복제거
-        # (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
-
-        results.append({
-            'model': "movies.movie",
-            'pk': len(results) + 1,
-            "fields": {
-            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
-            'title': title,
-            'overview': overview,
-            'release_date': release_date,
-            'popularity': popularity
-        }})
-
-    my_url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4dc86c92c350c5edac0c712116558e11&language=ko-KR'
-    response = requests.get(my_url)
-    movie_dict = response.json()
-    # return Response(movie_dict)
-
-    # 필요한 정보 골라내기
-    movie_list = movie_dict.get('results')
-    for movie in movie_list:
-        title = movie.get('original_title')
-        overview = movie.get('overview')
-        release_date = movie.get('release_date')
-        poster_path = movie.get('poster_path')
-        popularity = movie.get('popularity')
-
-        # # DB에 저장하기, 중복제거
-        # (movie, created) = Movie.objects.get_or_create(title=title, overview=overview, release_date=release_date, poster_path=poster_path, popularity=popularity)
-
-        results.append({
-            'model': "movies.movie",
-            'pk': len(results) + 1,
-            "fields": {
-            'poster_path': 'https://image.tmdb.org/t/p/w500' +  poster_path,
-            'title': title,
-            'overview': overview,
-            'release_date': release_date,
-            'popularity': popularity
-        }})
-
-    pprint.pprint(results)
-    with open('movies2.json', "w", encoding='utf-8') as f: # movies.json은 저장될 json 파일 명입니다., json_data은 저장할 데이터입니다.
-        json.dump(results, f, ensure_ascii=False)
-    return None
 
 
 @api_view(['GET'])
